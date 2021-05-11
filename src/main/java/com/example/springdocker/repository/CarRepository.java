cd7764bb60ee6e51b5f1381d740a80ebd56cd5d6
simonnojd <simon.nojd.jobb@gmail.com>
2021-05-11 10:12:41 +0200

    Added new files

---
+++ src/main/java/com/example/springdocker/repository/CarRepository.java
@@ -0,0 +1,13 @@
+package com.example.springdocker.repository;
+
+import com.example.springdocker.model.Car;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CarRepository extends MongoRepository<Car, String> {
+
+    List<Car> findCarByCanIDriveIt(boolean canDrive);
+}
