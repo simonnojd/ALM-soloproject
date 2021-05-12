26c85e0f77cf82a33abb5e20a43cb84a3ac5f106
simonnojd <simon.nojd.jobb@gmail.com>
2021-05-12 09:22:10 +0200

    Math stuff

--- src/main/java/MyMathCalc.java
+++ src/main/java/MyMathCalc.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class MyMathCalc {
+public class MyMathCalc {
+    public int add (int a, int b) {
+        return a + b;
+    }
+    public int multiply (int a, int b) {
+        return a * b;
+    }
+    public int divide (int a, int b) {
+        return a / b;
+    }
 }
